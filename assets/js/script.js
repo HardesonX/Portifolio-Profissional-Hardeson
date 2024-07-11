fc3080b6e665e8856cfe3a1f6ca15ff76a610b9a
Hardeson Dev <155669071+HardesonX@users.noreply.github.com>
2024-07-11 15:28:41 -0300

    upload do meu portifolio finalmente

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const BarMenu = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+
+
+
+BarMenu.addEventListener('click', ()=>{
+navbar.classList.toggle('active');
+});
